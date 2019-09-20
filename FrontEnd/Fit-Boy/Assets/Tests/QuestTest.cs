152089500d80f259ee26cc676bf9cb5a2569cd2c
Liam <fkd2190@autuni.ac.nz>
2019-09-20 19:50:02 +1200

    added quest test

---
+++ FrontEnd/Fit-Boy/Assets/Tests/QuestTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class QuestTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void QuestTestSimplePasses()
+        {
+            Make_quests mq = new Make_quests();
+            Quest quest = mq.Gen_Quest();
+            Assert.IsNotNull(quest);
+        }
+    }
+}
