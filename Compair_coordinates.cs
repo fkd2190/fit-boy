f43b72931d244919f4b9d7af998b84ca0d51cd04
beny11867 <53544771+beny11867@users.noreply.github.com>
2019-09-20 19:10:54 +1200

    Add files via upload

---
+++ Compair_coordinates.cs
@@ -0,0 +1,26 @@
+﻿package Fit_Boy_Quest_engine;
+public class Compair_coordinates
+{
+
+	public static bool Are_We_There_Yet(Coordinate current, Coordinate dest)
+	{
+		bool arrived = false;
+		if (((current.Lat
+					<= (dest.Lat + 0.0001))
+					&& (current.Lat
+					>= (dest.Lat - 0.0001))))
+		{
+			if (((current.Lon
+						<= (dest.Lon + 0.0001))
+						&& (current.Lon
+						>= (dest.Lon - 0.0001))))
+			{
+				arrived = true;
+			}
+
+		}
+
+		return arrived;
+	}
+}
+
