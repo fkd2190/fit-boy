bc1d0712d055718bb3010d3143d5eea5d98e4fac
JasonMartelletti <47404187+JasonMartelletti@users.noreply.github.com>
2019-10-18 19:19:02 +1300

    update

---
+++ FrontEnd/Fit-Boy/Assets/Scripts/CityScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CityScript : MonoBehaviour
+{
+    public bool knowsWeather;
+    public WeatherAPI weather;
+    public Button button;
+    public Sprite clouds;
+    public Sprite rain;
+    public Sprite clear;
+    public int lat = -36;
+    public int lon = 175;
+    Text cityText;
+    Text wind;
+    Text city;
+    Text desc;
+
+
+    void Start()
+    {
+        knowsWeather = false;
+        cityText = GameObject.Find("City").GetComponent<Text>();
+        wind = GameObject.Find("Wind").GetComponent<Text>();
+        city = GameObject.Find("Location").GetComponent<Text>();
+        desc = GameObject.Find("Desc").GetComponent<Text>();
+        weather = gameObject.AddComponent<WeatherAPI>();
+        weather.url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=f345b05b38a618ce356cc179bafec0e0";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!knowsWeather && weather.temp != 0)
+        {
+            cityText.text = (int)(weather.temp - 273.15) + " °C";
+            wind.text = "Wind speed: "+weather.wind;
+            city.text = "City: "+weather.city;
+            desc.text = weather.weatherDescription;
+            knowsWeather = true;
+            if(weather.weatherDescription.Contains("cloud"))
+            {
+                button.GetComponent<Image>().sprite = clouds;
+            }
+            else if(weather.weatherDescription.Contains("clear"))
+            {
+                button.GetComponent<Image>().sprite = clear;
+            }
+            else if (weather.weatherDescription.Contains("rain"))
+            {
+                button.GetComponent<Image>().sprite = rain;
+            }
+        }
+    }
+
+    void UpdateInfoPanel()
+    {
+
+    }
+}
---
+++ FrontEnd/Fit-Boy/Assets/Scripts/WeatherAPIScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class WeatherAPI : MonoBehaviour
+{
+    public string url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=f345b05b38a618ce356cc179bafec0e0";
+    public string city;
+    public string weatherDescription;
+    public float temp;
+    public float temp_min;
+    public float temp_max;
+    public float rain;
+    public float wind;
+    public float clouds;
+    
+    public void Start()
+    {
+        StartCoroutine(Starts());
+    }
+    // Use this for initialization
+    public IEnumerator Starts()
+    {
+        using (WWW request = new WWW(url))
+        {
+            yield return request;
+            if (request.error == null || request.error == "")
+            {
+                setWeatherAttributes(request.text);
+            }
+            else
+            {
+                Debug.Log("Error: " + request.error);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void setWeatherAttributes(string jsonString)
+    {
+        var weatherJson = JSON.Parse(jsonString);
+        city = weatherJson["name"].Value;
+        weatherDescription = weatherJson["weather"][0]["description"].Value;
+        temp = weatherJson["main"]["temp"].AsFloat;
+        temp_min = weatherJson["main"]["temp_min"].AsFloat;
+        temp_max = weatherJson["main"]["temp_max"].AsFloat;
+        rain = weatherJson["rain"]["3h"].AsFloat;
+        clouds = weatherJson["clouds"]["all"].AsInt;
+        wind = weatherJson["wind"]["speed"].AsFloat;
+    }
+}
